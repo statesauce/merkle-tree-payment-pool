34d70b42c4f7d867bb60158953450a64c40c6e07
xiphiness <xiphiness@protonmail.com>
2019-06-26 16:48:26 -0400

    update merklize, split up proof

--- merklize/src/index.js
+++ merklize/src/index.js
@@ -5,9 +5,7 @@ import util from 'util'
 import CumulativePaymentTree from '../../lib/cumulative-payment-tree.js'
 const web3 = new Web3(Web3.givenProvider || 'ws://some.local-or-remote.node:8546', null, {})
 let accounts = []
-let accountsL = []
 let payments = []
-let paymentsL = []
 var argv = require('yargs')
   .command(
       'make root-hash <ifile>',
@@ -18,24 +16,18 @@ var argv = require('yargs')
       (argv) => {
           let paymentsJson = JSON.parse(fs.readFileSync(argv.ifile))
           for (var addr in paymentsJson) {
-              payments.push({ payee: addr, amount: paymentsJson[addr] })
-              accounts.push(addr)
+              payments.push({ payee: addr.toLowerCase(), amount: paymentsJson[addr] })
+              accounts.push(addr.toLowerCase())
           }
           let merkleTree = new CumulativePaymentTree(payments)
           console.log("root: " + merkleTree.getHexRoot())
           console.log("payee: " + accounts[2])
-          console.log("proof: " + merkleTree.hexProofForPayee(accounts[2], 1))
-          for (var addr in paymentsJson) {
-              paymentsL.push({ payee: addr.toLowerCase(), amount: paymentsJson[addr] })
-              accountsL.push(addr.toLowerCase())
-          }
-          let merkleTreeL = new CumulativePaymentTree(paymentsL)
-          console.log("root: " + merkleTreeL.getHexRoot())
-          console.log("payee: " + accountsL[2])
-          console.log("payee: " + paymentsL[2].payee)
-          console.log("amount: " + paymentsL[2].amount)
-          console.log("proof: " + merkleTreeL.hexProofForPayee(accountsL[2], 1))
-
+          console.log("payee: " + payments[2].payee)
+          console.log("amount: " + payments[2].amount)
+          let proof = merkleTree.hexProofForPayee(accounts[2], 1)
+          console.log(proof)
+          console.log("payment cycle: " + proof.slice(0,66) + " amount: 0x" + proof.slice(66,130) )
+          console.log("proof: 0x" + proof.slice(130))
           process.exit(1)
       }
   ).command(
@@ -43,26 +35,26 @@ var argv = require('yargs')
       'generate payments for test accounts',
       (yargs) => {
           accounts = [
-              '0xa3a0080036b8dbc21f63da1189ef5355989255a2a40787a58c33ae63ed20e069',
-              '0x2b08f442dbed805005ada6feed49fdae6b3bcf9a5a0f1ee8349bdea9789ee772',
-              '0x2897dec6026dc93e64e787ef2d820d273d4d9141f066fc75bb209c52db0d8c39',
-              '0xbca7c4a5c1458e0dbbf7b64d3bcab00403c36caca1098defbdc2acc0d3278eca',
-              '0x494641c3d2ab250df050936c6304c783f8303137878fc3103f73c3f417789795',
-              '0xb3db90f160d9abe170e53d199d426489d7f3fa925c3521e11101777981b1f99f',
-              '0xeb086d581d30628bec7424f910df4388d67623e77cae465fd7b3f07ea3018e46',
-              '0xcdfa5086cc6ef4f9f259ec3065b0ac2a41093f359168c8b2a544dc56f23bb0e2',
-              '0xb3f37cdb4b990f6a0c1e88e12dc3b3ba9a6cf79cc49f1807b1678f0bf0a0eaef',
-              '0x52c97e409618367e81f559255fb01a10d68fc4edbf22c51f459a61fddef8dfc5']
+              "0xfcc60fbb5598c19004f29c6487c175a7832ee7882cf24ac1b8fef6c4717dd8b6",
+              "0x3d8ad8e934d6899f9278e70e13cc8f462a24adbf6397f4bc3a715a245744595d",
+              "0x3c0a1c4f289db9c96e3335315124821a9b06d28683a757c7d22d6415da0037a3",
+              "0x773977da4f39e77d77e72de87714ce2216dc9a70582ec9bfb1276ceb3fe3961f",
+              "0x072de7e161f33d1fd4027f91ba85d1f357a980e8c88f2773ac116550d4966177",
+              "0x40cdb70fd7bb9f808bcc98ce3fb70d584f81003f8589f6fcf2d3a8bfb2a809f5",
+              "0x6fa7f13af7ca213557f26e73b07578f4d6639dededfddf73e137b1a65606fa55",
+              "0xea315cc365d3c08d75d210011032554212f271341551ae44874ada02a00461fd",
+              "0x42aae8752b233a67c7e505ed7a366f4fdf428f648064561556d7b1e7d09621c7",
+              "0x5707b3ef7d70af96689fc14c3a9551f23f35a746bea6e3d8632a9379d4582c4b" ]
       },
       (argv) => {
           accounts = accounts.map((val) => {
-               return web3.eth.accounts.privateKeyToAccount(val).address
+              return web3.eth.accounts.privateKeyToAccount(val).address
           })
-          console.log(accounts)
+          //console.log(accounts)
           let pay_values = [0, 0, 10, 12, 2, 1, 32, 10, 9, 101]
           let paymentsJson = {}
           accounts.reduce((_, val, index, array) => {
-              console.log("addr: " + val + "amount: " + pay_values[index] + ", index: "+  index)
+              console.log("addr: " + val + " amount: " + pay_values[index] + ", index: "+  index)
               paymentsJson[val] = pay_values[index]
           }, accounts[0])
           fs.writeFileSync(argv.ofile, JSON.stringify(paymentsJson, null, 2))
