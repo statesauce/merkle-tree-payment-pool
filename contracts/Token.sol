7dc5752f9f3c652144d13ac7014747d1392d9ae5
xiphiness <xiphiness@protonmail.com>
2019-06-29 16:10:55 -0400

    contracts compiling, a large number of tests compiling

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,5 +1,5 @@
 pragma solidity ^0.5.6;
-import '../zeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol';
+import './openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol';
 
 // Generic mintable ERC-20 token used by the tests
 contract Token is ERC20Mintable { }
