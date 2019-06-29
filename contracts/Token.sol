89f8acd1820e86865b2c61fb4636751fc72539d2
sethfork <sefeibus@gmail.com>
2019-06-29 19:04:56 -0400

    Restructure project into lerna monorepo

--- contracts/Token.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.5.6;
-import './openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol';
-
-// Generic mintable ERC-20 token used by the tests
-contract Token is ERC20Mintable { }
